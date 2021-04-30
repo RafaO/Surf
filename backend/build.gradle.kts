a42186c65d321cb72c888e7b22c8bb7f626f9778
Rafael Ortega <rafao1989@hotmail.com>
2021-04-30 22:26:46 +0200

    transform backend to multiplatform project with jvm target

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,12 +1,34 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinCommonOptions
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilationToRunnableFiles
+
 plugins {
-    id("kotlin-platform-jvm")
+    kotlin("multiplatform")
     application
 }
 
-dependencies {
-    implementation(project(":shared"))
+kotlin {
+    jvm()
+
+    sourceSets {
+        val jvmMain by getting {
+            kotlin.srcDir("src")
+            resources.srcDir("resources")
+
+            dependencies {
+                implementation(project(":shared"))
+            }
+        }
+        val jvmTest by getting { }
+    }
 }
 
-application {
-    mainClass.set("ServerKt")
+task<JavaExec>("runServer") {
+    main = "ServerKt"
+    val jvm by kotlin.targets.getting
+    val main: KotlinCompilation<KotlinCommonOptions> by jvm.compilations
+
+    val runtimeDependencies =
+        (main as KotlinCompilationToRunnableFiles<KotlinCommonOptions>).runtimeDependencyFiles
+    classpath = files(main.output.allOutputs, runtimeDependencies)
 }
